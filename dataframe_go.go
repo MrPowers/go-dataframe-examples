b8974536d7be4bdf2b1ab256640ed999c5e893f5
MrPowers <matthewkevinpowers@gmail.com>
2020-03-25 11:45:17 -0500

    uncomment failing code

--- dataframe_go.go
+++ dataframe_go.go
@@ -20,26 +20,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// step 2: read the csv into memory.
-	//reader := csv.NewReader(csvfile)
-	//records, err := reader.ReadAll()
-	//if err != nil {
-	//log.Fatal(err)
-	//}
-
 	dataframe, err := imports.LoadFromCSV(ctx, csvfile)
 
 	fmt.Print(dataframe.Table())
 
-	//s := dataframe.Series[2]
+	s := dataframe.Series[2]
 
-	//applyFn := dataframe.ApplySeriesFn(func(val interface{}, row, nRows int) interface{} {
-	//return 2 * val.(int64)
-	//})
+	applyFn := dataframe.ApplySeriesFn(func(val interface{}, row, nRows int) interface{} {
+		return 2 * val.(int64)
+	})
 
-	//dataframe.Apply(ctx, s, applyFn, dataframe.FilterOptions{InPlace: true})
+	dataframe.Apply(ctx, s, applyFn, dataframe.FilterOptions{InPlace: true})
 
-	//fmt.Print(dataframe.Table())
+	fmt.Print(dataframe.Table())
 
 	// looks like this file has relevant stuff: https://github.com/rocketlaunchr/dataframe-go/blob/0ec0a97d0f0d052e95557fee2aba1f3ce5bf94b1/apply.go
 
