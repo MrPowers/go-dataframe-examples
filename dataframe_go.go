361488f9abd8b3e7f41711ac4a44e9082b7b635b
MrPowers <matthewkevinpowers@gmail.com>
2020-03-25 11:36:50 -0500

    WIP

--- dataframe_go.go
+++ dataframe_go.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"encoding/csv"
+	//"encoding/csv"
+	"context"
 	"fmt"
+	//"github.com/rocketlaunchr/dataframe-go/dataframe"
 	"github.com/rocketlaunchr/dataframe-go/imports"
 	"log"
 	"os"
-	"strings"
+	//"strings"
 )
 
 func main() {
+	ctx := context.TODO()
+
 	// step 1: open the csv
 	csvfile, err := os.Open("data/example.csv")
 	if err != nil {
@@ -17,14 +21,28 @@ func main() {
 	}
 
 	// step 2: read the csv into memory.
-	reader := csv.NewReader(csvfile)
-	records, err := reader.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//reader := csv.NewReader(csvfile)
+	//records, err := reader.ReadAll()
+	//if err != nil {
+	//log.Fatal(err)
+	//}
 
-	df, err := imports.LoadFromCSV(ctx, strings.NewReader(records))
+	dataframe, err := imports.LoadFromCSV(ctx, csvfile)
 
-	fmt.Print(df.Table())
+	fmt.Print(dataframe.Table())
+
+	//s := dataframe.Series[2]
+
+	//applyFn := dataframe.ApplySeriesFn(func(val interface{}, row, nRows int) interface{} {
+	//return 2 * val.(int64)
+	//})
+
+	//dataframe.Apply(ctx, s, applyFn, dataframe.FilterOptions{InPlace: true})
+
+	//fmt.Print(dataframe.Table())
+
+	// looks like this file has relevant stuff: https://github.com/rocketlaunchr/dataframe-go/blob/0ec0a97d0f0d052e95557fee2aba1f3ce5bf94b1/apply.go
+
+	// can't figure it out
 
 }
--- qframe.go
+++ qframe.go
@@ -1,32 +1,39 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"github.com/tobgu/qframe"
+	"github.com/tobgu/qframe/function"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
-	//input := `COL1,COL2
-	//a,1.5
-	//b,2.25
-	//c,3.0`
-
-	csvfile, err := os.Open("data/example.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	reader := csv.NewReader(csvfile)
-	// note: you can also iterate through csv records with `.Read()` to handle row-level errors / manipulate data
-	records, err := reader.ReadAll()
+	// open a CSV file
+	csvfile, err := os.Open("data/basic.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	f := qframe.ReadCSV(strings.NewReader(input))
+	// view the DataFrame
+	f := qframe.ReadCSV(csvfile)
 	fmt.Println(f)
+
+	// concatenate two DataFrame columns
+	f = f.Apply(
+		qframe.Instruction{
+			Fn:      function.ConcatS,
+			DstCol:  "col3",
+			SrcCol1: "col1",
+			SrcCol2: "col2"})
+
+	fmt.Println(f)
+
+	// write the DataFrame
+	file, err := os.Create("tmp/qframe_ouput.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.ToCSV(file)
 
 }
